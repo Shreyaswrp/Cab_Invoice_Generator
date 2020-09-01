2a04060cce26933f5f812eb69f0934f625cbb286
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-01 23:01:44 +0530

    Refactored to add negative test cases

--- src/test/java/com/InvoiceGenerator/InvoiceServiceTest.java
+++ src/test/java/com/InvoiceGenerator/InvoiceServiceTest.java
@@ -2,13 +2,17 @@ package com.InvoiceGenerator;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class InvoiceServiceTest {
 
     InvoiceService invoiceService = null;
     private RideRepository rideRepository = null;
     Ride[] rides = null;
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void setup() {
@@ -26,12 +30,28 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25, totalFare, 0.0);
     }
 
+    @Test(expected = AssertionError.class)
+    public void givenDistanceAndTime_WhenExpectedTotalFareIsWrongForTypeNormal_ShouldThrowAssertionError() {
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = CabRide.Normal.calculateTotalFare(new Ride(distance, time));
+        Assert.assertEquals(100, totalFare, 0.0);
+    }
+
     @Test
     public void givenDistanceAndTime_WhenPremium_ShouldReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
         double totalFare = CabRide.Premium.calculateTotalFare(new Ride(distance, time));
         Assert.assertEquals(40, totalFare, 0.0);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void givenDistanceAndTime_WhenExpectedTotalFareIsWrongForTypePremium_ShouldThrowAssertionError() {
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = CabRide.Premium.calculateTotalFare(new Ride(distance, time));
+        Assert.assertEquals(100, totalFare, 0.0);
     }
 
     @Test
@@ -42,6 +62,14 @@ public class InvoiceServiceTest {
         Assert.assertEquals(5, totalFare, 0.0);
     }
 
+    @Test(expected = AssertionError.class)
+    public void givenLessDistanceAndTime_WhenExpectedMinimumFareIsWrongForTypeNormal_ShouldThrowAssertionError() {
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = CabRide.Normal.calculateFare(new Ride(distance, time));
+        Assert.assertEquals(100, totalFare, 0.0);
+    }
+
     @Test
     public void givenLessDistanceAndTime_WhenPremium_ShouldReturnMinimumFare() {
         double distance = 0.1;
@@ -49,10 +77,26 @@ public class InvoiceServiceTest {
         double totalFare = CabRide.Premium.calculateFare(new Ride(distance, time));
         Assert.assertEquals(20, totalFare, 0.0);
     }
+
+    @Test(expected = AssertionError.class)
+    public void givenLessDistanceAndTime_WhenExpectedMinimumFareIsWrongForTypePremium_ShouldThrowAssertionError() {
+        double distance = 2.0;
+        int time = 5;
+        double totalFare = CabRide.Premium.calculateFare(new Ride(distance, time));
+        Assert.assertEquals(100, totalFare, 0.0);
+    }
+
     @Test
     public void givenMultipleRides_WhenNormal_ShouldReturnInvoiceSummary() {
         InvoiceSummary invoiceDetails = invoiceService.generateInvoice(rides, CabRide.Normal);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedInvoiceSummary, invoiceDetails);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void givenMultipleRides_WhenExpectedInvoiceSummaryIsWrongForTypeNormal_ShouldThrowAssertionError() {
+        InvoiceSummary invoiceDetails = invoiceService.generateInvoice(rides, CabRide.Normal);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 300);
         Assert.assertEquals(expectedInvoiceSummary, invoiceDetails);
     }
 
@@ -61,6 +105,13 @@ public class InvoiceServiceTest {
         InvoiceSummary invoiceSummary = invoiceService.generateInvoice(rides, CabRide.Premium);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 60);
         Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+    }
+
+    @Test(expected = AssertionError.class)
+    public void givenMultipleRides_WhenExpectedInvoiceSummaryIsWrongForTypePremium_ShouldThrowAssertionError() {
+        InvoiceSummary invoiceDetails = invoiceService.generateInvoice(rides, CabRide.Premium);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 300);
+        Assert.assertEquals(expectedInvoiceSummary, invoiceDetails);
     }
 
     @Test
@@ -72,12 +123,66 @@ public class InvoiceServiceTest {
         Assert.assertEquals(expectedSummary, invoiceSummary);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void givenUserIdAndRides_WhenUserIdIsWrongForTypeNormal_ShouldThrowNullPointerException() {
+        String userId = "shreyaswrp";
+        rideRepository.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary("shreyaswaroop", CabRide.Normal);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenUserIdAndRides_WhenUserIdIsNullForTypeNormal_ShouldThrowNullPointerException() {
+        String userId = "shreyaswrp";
+        rideRepository.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary(null, CabRide.Normal);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenUserIdAndRides_WhenUserIdIsEmptyForTypeNormal_ShouldThrowNullPointerException() {
+        String userId = "shreyaswrp";
+        rideRepository.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary(" ", CabRide.Normal);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
     @Test
     public void givenUserIdAndRides_WhenPremiumType_ShouldReturnInvoiceSummary() {
         String userId = "shreyaswrp";
         rideRepository.addRides(userId, rides);
         InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary(userId, CabRide.Premium);
         InvoiceSummary expectedSummary = new InvoiceSummary(2, 60);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenUserIdAndRides_WhenUserIdIsWrongForTypePremium_ShouldThrowNullPointerException() {
+        String userId = "shreyaswrp";
+        rideRepository.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary("shreyaswaroop", CabRide.Premium);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenUserIdAndRides_WhenUserIdIsNullForTypePremium_ShouldThrowNullPointerException() {
+        String userId = "shreyaswrp";
+        rideRepository.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary(null, CabRide.Premium);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void givenUserIdAndRides_WhenUserIdIsEmptyForTypePremium_ShouldThrowNullPointerException() {
+        String userId = "shreyaswrp";
+        rideRepository.addRides(userId, rides);
+        InvoiceSummary invoiceSummary = invoiceService.getInvoiceSummary(" ", CabRide.Premium);
+        InvoiceSummary expectedSummary = new InvoiceSummary(2, 30);
         Assert.assertEquals(expectedSummary, invoiceSummary);
     }
 }
